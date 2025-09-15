583718fe17bf649f6d335049eac5e72e1d2a1d9f
Daulongjayvee <jayveeadino@gmail.com>
2025-09-15 23:02:02 +0800

    css updated

--- resources/views/adviser/reports.blade.php
+++ resources/views/adviser/reports.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Adviser Dashboard - Reports</title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"/>
-
+  <link rel="stylesheet" href="{{ asset('css/adviser/reports.css') }}">
   <style>
     :root {
       --primary-color: #000000;
